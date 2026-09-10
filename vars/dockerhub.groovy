1f49f9de031c967f6c5e8ad7a5c385960857e4b5
Yash Thorat <work.yashthorat29@gmail.com>
2026-09-10 15:20:29 +0530

    leo betrayed

--- vars/dockerhub.groovy
+++ vars/dockerhub.groovy
@@ -1,6 +1,6 @@
 def call(String cred , String username, String image_name , String tag){
   echo 'pushing to dockerhub'
-  withCredentials([usernamePassword(credentialId:cred, usernameVariable: 'dockerhubuser', passwordVariable: 'dockerhubpass' )]){
+  withCredentials([usernamePassword(credentialsId:cred, usernameVariable: 'dockerhubuser', passwordVariable: 'dockerhubpass' )]){
   sh "docker login -u $dockerhubuser -p $dockerhubpass"
   sh "docker image tag ${image_name}:${tag} ${username}/${image_name}:${tag}"
   sh "docker push ${username}/${image_name}:${tag}" 
