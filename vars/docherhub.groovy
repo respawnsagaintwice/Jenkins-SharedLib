8931f1891c130f6685c52e6a59984a44bd459a50
Yash Thorat <work.yashthorat29@gmail.com>
2026-09-10 14:55:35 +0530

    Update docherhub.groovy

--- vars/docherhub.groovy
+++ vars/docherhub.groovy
@@ -2,7 +2,7 @@ def call(String cred , String username, String image_name , String tag){
   echo 'pushing to dockerhub'
   withCredentials([usernamePassword(credentialId:cred, usernameVariable: 'dockerhubuser', passwordVariable: 'dockerhubpass' )]){
   sh "docker login -u $dockerhubuser -p $dockerhubpass"
-  sh "docker images tag ${image_name}:${tag} ${username}/${image_name}:${tag}"
+  sh "docker image tag ${image_name}:${tag} ${username}/${image_name}:${tag}"
   sh "docker push ${username}/${image_name}:${tag}" 
   }
   echo 'pushed to dockerhub'
